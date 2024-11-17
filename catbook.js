d600b9b37132f9b4bdcc28f6792487afb710392d
tanner.caffrey <tanner.caffrey@gmail.com>
2024-11-17 14:22:46 -0500

    Update scrapbook to work with new api

--- catbook.js
+++ catbook.js
@@ -1,41 +1,44 @@
 async function fetchPhotos() {
-    fetch('https://gwynnie.gay/photos/photos.json')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data); // The parsed JSON data
-      // Now you can use `data` to populate your site content
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
+  return fetch('https://api.gwynnie.gay/photos/photos.json')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok ' + response.statusText);
+          }
+          return response.json();
+      })
+      .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+          throw error; // Ensure the error propagates
+      });
 }
 
-async function generatePhotoElements() {
-    const photosObj = await fetchPhotos();
-    const container = document.getElementById('photoContainer');
 
-    photosObj.photos.forEach(photo => {
-        const photoDiv = document.createElement('div');
-        photoDiv.id = photo.filename.replace(/\.[^/.]+$/, ""); // Removing the file extension for the id
-        photoDiv.className = 'scrapbookPhoto';
-        
-        const imgElement = document.createElement('img');
-        imgElement.src = `photos/${photo.name}`;
-
-        const h3Element = document.createElement('h3');
-        h3Element.textContent=`${photo.description}`
-
-        photoDiv.toggleAttribute('can-move');
-
-        photoDiv.appendChild(imgElement);
-        photoDiv.appendChild(h3Element);
-        container.appendChild(photoDiv);
-    });
+function generatePhotoElements() {
+    fetchPhotos().then(
+      photos => {
+        console.log("Fetched photos object in generatePhotoElements:", photos);
+        const container = document.getElementById('photoContainer');
+    
+        photos.photos.forEach(photo => {
+            const photoDiv = document.createElement('div');
+            photoDiv.id = photo.title; // Removing the file extension for the id
+            photoDiv.className = 'scrapbookPhoto';
+            
+            const imgElement = document.createElement('img');
+            imgElement.src = `https://api.gwynnie.gay/photos/${photo.filename}`;
+    
+            const h3Element = document.createElement('h2');
+            h3Element.textContent=`${photo.description}`
+    
+            photoDiv.toggleAttribute('can-move');
+    
+            photoDiv.appendChild(imgElement);
+            photoDiv.appendChild(h3Element);
+            container.appendChild(photoDiv);
+        })
+      }
+    )
+    
     return Promise.resolve();
 }
 
@@ -51,7 +54,7 @@ function loadExternalScript(url) {
 }
 
 async function main() {
-    await generatePhotoElements();
+    generatePhotoElements();
     try {
         const module = await import('https://unpkg.com/playhtml@latest/dist/init.es.js'); // Replace with the actual URL of the external module
         console.log('Photos have been generated and external module loaded.', module);
